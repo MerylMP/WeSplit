04b5c258b5459a497613607db90c657dbaadf60f
Maria <maria.mphob@gmail.com>
2019-10-31 12:25:39 +0100

    code and interface improvements

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,16 +14,25 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     
+    var numberOfPeople2 = 2.0
     let tipPercentages = [10, 15, 20, 25, 0]
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    
+    var peopleCount: Double {
+        Double(numberOfPeople + 2)
+    }
+
+    var totalAmount: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
         
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let amountPerPerson = totalAmount / peopleCount
         return amountPerPerson
     }
     
@@ -48,6 +57,10 @@ struct ContentView: View {
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header:Text("Total amount for the check")){
+                    Text("$\(totalAmount, specifier: "%.2f")")
                 }
                 
                 Section (header:Text("Amount per person")){
